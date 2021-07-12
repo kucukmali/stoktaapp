520109bfa9851b957ae41922b700d0e0849c51e5
kucukmali <kucuk.mali94@gmail.com>
2021-07-12 17:45:05 +0300

    New Version

--- stoktaapp/lib/pages/home_screen.dart
+++ stoktaapp/lib/pages/home_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget {
@@ -9,48 +10,72 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.white,
         body: Container(
           margin: EdgeInsets.all(15),
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.start,
+          padding: EdgeInsets.only(left: 15, top: 15, right: 15),
+          child: Column(
             children: [
-              Column(
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
-                  Text(
-                    'Hey Küçükali!',
-                    style: (TextStyle(
-                        fontWeight: FontWeight.w800,
-                        fontSize: 24,
-                        color: Colors.black)),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        'Hey Küçükali!',
+                        style: (TextStyle(
+                            fontWeight: FontWeight.w800,
+                            fontSize: 19,
+                            color: Colors.black)),
+                      ),
+                      Text(
+                        'Lets Find Something!',
+                        style: (TextStyle(
+                            color: Colors.black38,
+                            fontWeight: FontWeight.w300,
+                            fontSize: 15)),
+                      ),
+                    ],
                   ),
-                  Text(
-                    'Lets Find Something!',
-                    style: (TextStyle(
-                        color: Colors.black38,
-                        fontWeight: FontWeight.w300,
-                        fontSize: 18)),
-                  ),
-                ],
-              ),
-              Column(
-                children: [
                   Container(
-                    width: 100,
-                    height: 40,
-                    decoration: BoxDecoration(color: Colors.pink,borderRadius: BorderRadius.circular(20)),
-                  )
+                    height: 36,
+                    width: 36,
+                    decoration: BoxDecoration(
+                      color: Colors.orange,
+                      borderRadius: BorderRadius.circular(6),
+                    ),
+                  ),
                 ],
               ),
               Container(
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    CircleAvatar(
-                      backgroundColor: Colors.orange,
-                    )
-                  ],
-                ),
-              ),
+                  padding: EdgeInsets.only(left: 10),
+                  margin: EdgeInsets.symmetric(vertical: 35),
+                  width: size.width * .65,
+                  height: 30,
+                  decoration: BoxDecoration(
+                      boxShadow: [
+                        BoxShadow(
+                          color: Colors.grey.withOpacity(0.15),
+                          spreadRadius: 3,
+                          blurRadius: 5,
+                          offset: Offset(0, 3), // changes position of shadow
+                        ),
+                      ],
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(6)),
+                  child: Container(
+                    child: TextField(
+                      textAlignVertical: TextAlignVertical.bottom,
+                      decoration: InputDecoration(
+                          border: InputBorder.none,
+                          focusedBorder: InputBorder.none,
+                          enabledBorder: InputBorder.none,
+                          errorBorder: InputBorder.none,
+                          disabledBorder: InputBorder.none,
+                          contentPadding:
+                              EdgeInsets.only(left: -5, bottom: 11, top: 8),
+                          hintText: "Search"),
+                    ),
+                  ))
             ],
           ),
         ),
